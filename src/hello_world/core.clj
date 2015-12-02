36e76226ea1e52324bdec041846b607208e4eaa8
Alvaro Garcia <derecerca@gmail.com>
2015-12-03 00:20:31 +0100

    refactor: extract explanatory variables

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -20,7 +20,8 @@
       :query-params [op]
       :summary "x+y with query-parameters"
       (let [operands (range start (inc end))
-            operators (map (fn [_] (cond "sum" +)) op)]
-      (ok {:total operands :op (map #(apply % operands) operators)})))
+            operators (map (fn [_] (cond "sum" +)) op)
+            results (map #(apply % operands) operators)]
+      (ok {:total operands :op results})))
 ))
 
