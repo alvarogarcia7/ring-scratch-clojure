840fb4fc6ed33ef4f8497b898b53e6ce6b52ea37
Alvaro Garcia <derecerca@gmail.com>
2015-12-02 21:47:44 +0100

    add sample ring project file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject ring-scratch "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]])
