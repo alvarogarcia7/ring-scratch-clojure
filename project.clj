526b9648d0e7880b8ec5365eec59784c0ca5b0e4
Alvaro Garcia <derecerca@gmail.com>
2015-12-03 22:02:38 +0100

    add first test

--- project.clj
+++ project.clj
@@ -6,5 +6,13 @@
   :profiles {:uberjar { :resource-paths ["swagger-ui"]
                                 :aot :all}
              :dev {
-                    :dependencies [[javax.servlet/servlet-api "2.5"]]
+                    :dependencies [
+                                              [javax.servlet/servlet-api "2.5"] 
+                                              [ring/ring-mock "0.3.0"]
+                                              [midje "1.8.2"]
+                                              [peridot "0.4.1"]
+                                              [compojure "1.4.0"]
+                                              [metosin/compojure-api "0.23.0"]
+                                               [cheshire "5.5.0"]
+                                            ]
                     :plugins [[lein-ring "0.9.6"]]}})
---
+++ test/hello_world/core_test.clj
@@ -0,0 +1,51 @@
+(ns hello-world.core-test
+  (:require [compojure.api.sweet :refer :all]
+                [cheshire.core :as cheshire]
+                [midje.sweet :refer :all]
+                [peridot.core :as p]
+                [hello-world.core :as hw]
+)
+  (:import [java.io InputStream]))
+
+(defn json [x] (cheshire/generate-string x))
+
+(defn read-body [body]
+  (if (instance? InputStream body)
+    (slurp body)
+    body))
+
+(defn follow-redirect [state]
+  (if (some-> state :response :headers (get "Location"))
+    (p/follow-redirect state)
+    state))
+
+(defn raw-get* [app uri & [params headers]]
+  (let [{{:keys [status body headers]} :response}
+        (-> (p/session app)
+            (p/request uri
+                       :request-method :get
+                       :params (or params {})
+                       :headers (or headers {}))
+            follow-redirect)]
+    [status (read-body body) headers]))
+
+(defn parse-body [body]
+  (let [body (read-body body)
+        body (if (instance? String body)
+               (cheshire/parse-string body true)
+               body)]
+    body))
+
+(defn get* [app uri & [params headers]]
+  (let [[status body headers]
+        (raw-get* app uri params headers)]
+    [status (parse-body body) headers]))
+
+
+(facts "x"
+(fact "y"
+  (let [app hw/app]
+    (fact "default parameters"
+      (let [[status body] (get* app "/math/range/1/3?op=sum")]
+        status => 200
+        body =>{:total [1,2,3] :results [{:sum 6}], :op ["sum"]})))))
